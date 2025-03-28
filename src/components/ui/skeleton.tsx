5dab5ecddea09dc10d9bef3a3a4242691b989281
contigen <69436315+contigen@users.noreply.github.com.>
2025-03-28 20:19:40 +0100

    Add UI components

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,10 +1,13 @@
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils'
 
-function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+function Skeleton({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div
-      data-slot="skeleton"
-      className={cn("bg-accent animate-pulse rounded-md", className)}
+      data-slot='skeleton'
+      className={cn(
+        'bg-stone-100 dark:bg-zinc-800  animate-pulse rounded-xl',
+        className
+      )}
       {...props}
     />
   )
